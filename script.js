74305b4eae2195b009d77c3c6b70c95100e935e8
David Middleton <middleton.davida@gmail.com>
2022-01-07 15:49:42 -0500

    added leading zeros to hours, minutes, and seconds functions

--- script.js
+++ script.js
@@ -3,56 +3,60 @@ function getTime() {
     return date;
 }
 
+
 function hours() {
     let hours = getTime().getHours();
     switch (hours) {
         case 13:
-            hours = 1;
+            hours = "1";
             break;
         case 14:
-            hours = 2;
+            hours = '2';
             break;
         case 15:
-            hours = 3;
+            hours = '3';
             break;
         case 16:
-            hours = 4;
+            hours = '4';
             break;
         case 17:
-            hours = 5;
+            hours = '5';
             break;
         case 18:
-            hours = 6;
+            hours = '6';
             break;
         case 19:
-            hours = 7;
+            hours = '7';
             break;
         case 20:
-            hours = 8;
+            hours = '8';
             break;
         case 21:
-            hours = 9;
+            hours = '9';
             break;
         case 22:
-            hours = 10;
+            hours = '10';
             break;
         case 23:
-            hours = 11;
+            hours = '11';
             break;
         case 24:
-            hours = 12;
+            hours = '12';
             break;
-    }
+    };
+    hours = hours.toString().padStart(2, '0');
     return hours;
 }
 
 function minutes() {
     let minutes = getTime().getMinutes();
+    minutes = minutes.toString().padStart(2, '0');
     return minutes;
 }
 
 function seconds() {
     let seconds = getTime().getSeconds();
+    seconds = seconds.toString().padStart(2, '0');
     return seconds;
 }
 
@@ -61,5 +65,3 @@ console.log(hours());
 console.log(minutes());
 console.log(seconds());
 
-// const minutes = date.getMinutes();
-// const seconds = date.getSeconds();
